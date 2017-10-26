f387a27319c8ba009c7d3d9d18c04f9d779ade14
Илья Остроух <ilya.ostrouh@yandex.ru>
2017-10-26 23:53:28 +0300

    дизайн супер

--- ServerApp/ClientApp/Chat_form.cs
+++ ServerApp/ClientApp/Chat_form.cs
@@ -31,5 +31,10 @@ namespace ClientApp
         {
             this.Close();
         }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
