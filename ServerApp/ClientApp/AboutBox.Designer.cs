133f3e74391acffd3ae692a5ca55d0cbf0ba7234
Илья Остроух <ilya.ostrouh@yandex.ru>
2017-12-17 22:07:00 +0300

    о программе, переименовал лабелы

--- ServerApp/ClientApp/AboutBox.Designer.cs
+++ ServerApp/ClientApp/AboutBox.Designer.cs
@@ -29,13 +29,13 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.okButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.name_label = new System.Windows.Forms.Label();
+            this.version_label = new System.Windows.Forms.Label();
+            this.rights_label = new System.Windows.Forms.Label();
+            this.nameOrg_label = new System.Windows.Forms.Label();
+            this.description_label = new System.Windows.Forms.Label();
             this.tableLayoutPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
             this.SuspendLayout();
@@ -47,11 +47,11 @@
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67F));
             this.tableLayoutPanel.Controls.Add(this.logoPictureBox, 1, -1);
             this.tableLayoutPanel.Controls.Add(this.okButton, 1, 6);
-            this.tableLayoutPanel.Controls.Add(this.label2, 1, 0);
-            this.tableLayoutPanel.Controls.Add(this.label3, 1, 1);
-            this.tableLayoutPanel.Controls.Add(this.label4, 1, 2);
-            this.tableLayoutPanel.Controls.Add(this.label5, 1, 3);
-            this.tableLayoutPanel.Controls.Add(this.label1, 1, 4);
+            this.tableLayoutPanel.Controls.Add(this.name_label, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.version_label, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.rights_label, 1, 2);
+            this.tableLayoutPanel.Controls.Add(this.nameOrg_label, 1, 3);
+            this.tableLayoutPanel.Controls.Add(this.description_label, 1, 4);
             this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel.Location = new System.Drawing.Point(9, 9);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
@@ -68,64 +68,6 @@
             this.tableLayoutPanel.Size = new System.Drawing.Size(424, 170);
             this.tableLayoutPanel.TabIndex = 0;
             // 
-            // okButton
-            // 
-            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.okButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.okButton.Location = new System.Drawing.Point(341, 142);
-            this.okButton.Name = "okButton";
-            this.okButton.Size = new System.Drawing.Size(80, 25);
-            this.okButton.TabIndex = 24;
-            this.okButton.Text = "&ОК";
-            this.okButton.Click += new System.EventHandler(this.okButton_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(142, 70);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(213, 39);
-            this.label1.TabIndex = 25;
-            this.label1.Text = "The best chat in the world \r\nГлава Backend: Котельников Владислав\r\nГлава Frontend" +
-    ": Остроух Илья\r\n";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(142, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 13);
-            this.label2.TabIndex = 26;
-            this.label2.Text = "Название продукта: ЧАТ";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(142, 16);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 13);
-            this.label3.TabIndex = 27;
-            this.label3.Text = "Версия: 13.12.2017";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(142, 31);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(153, 13);
-            this.label4.TabIndex = 28;
-            this.label4.Text = "Авторские права защищены";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(142, 48);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(173, 13);
-            this.label5.TabIndex = 29;
-            this.label5.Text = "Название организации: VladIl Co";
-            // 
             // logoPictureBox
             // 
             this.logoPictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.BackgroundImage")));
@@ -139,6 +81,64 @@
             this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.logoPictureBox.TabIndex = 12;
             this.logoPictureBox.TabStop = false;
+            // 
+            // okButton
+            // 
+            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.okButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.okButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.okButton.Location = new System.Drawing.Point(341, 142);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(80, 25);
+            this.okButton.TabIndex = 24;
+            this.okButton.Text = "&ОК";
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // name_label
+            // 
+            this.name_label.AutoSize = true;
+            this.name_label.Location = new System.Drawing.Point(142, 0);
+            this.name_label.Name = "name_label";
+            this.name_label.Size = new System.Drawing.Size(134, 13);
+            this.name_label.TabIndex = 26;
+            this.name_label.Text = "Название продукта: ЧАТ";
+            // 
+            // version_label
+            // 
+            this.version_label.AutoSize = true;
+            this.version_label.Location = new System.Drawing.Point(142, 16);
+            this.version_label.Name = "version_label";
+            this.version_label.Size = new System.Drawing.Size(104, 13);
+            this.version_label.TabIndex = 27;
+            this.version_label.Text = "Версия: 13.12.2017";
+            // 
+            // rights_label
+            // 
+            this.rights_label.AutoSize = true;
+            this.rights_label.Location = new System.Drawing.Point(142, 31);
+            this.rights_label.Name = "rights_label";
+            this.rights_label.Size = new System.Drawing.Size(153, 13);
+            this.rights_label.TabIndex = 28;
+            this.rights_label.Text = "Авторские права защищены";
+            // 
+            // nameOrg_label
+            // 
+            this.nameOrg_label.AutoSize = true;
+            this.nameOrg_label.Location = new System.Drawing.Point(142, 48);
+            this.nameOrg_label.Name = "nameOrg_label";
+            this.nameOrg_label.Size = new System.Drawing.Size(173, 13);
+            this.nameOrg_label.TabIndex = 29;
+            this.nameOrg_label.Text = "Название организации: VladIl Co";
+            // 
+            // description_label
+            // 
+            this.description_label.AutoSize = true;
+            this.description_label.Location = new System.Drawing.Point(142, 70);
+            this.description_label.Name = "description_label";
+            this.description_label.Size = new System.Drawing.Size(213, 39);
+            this.description_label.TabIndex = 25;
+            this.description_label.Text = "The best chat in the world \r\nГлава Backend: Котельников Владислав\r\nГлава Frontend" +
+    ": Остроух Илья\r\n";
             // 
             // AboutBox
             // 
@@ -168,11 +168,11 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
         private System.Windows.Forms.Button okButton;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label description_label;
+        private System.Windows.Forms.Label name_label;
+        private System.Windows.Forms.Label version_label;
+        private System.Windows.Forms.Label rights_label;
+        private System.Windows.Forms.Label nameOrg_label;
         private System.Windows.Forms.PictureBox logoPictureBox;
     }
 }
